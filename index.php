70364f27fc2f3494421547cde8da7436097fe603
Leon Zethof <leonz.ionic@gmail.com>
2019-05-29 16:24:14 +0200

    ik ben er ook

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
   echo "Change";
   echo "User-2 was here, all base are belong to us";
   echo "rra-enz.. was also here, I take base belong to me";
+  echo "Leon is hier";
 <<<<<<< HEAD
   echo "olaf was hier";
 
