1fdfc82678d62407d31bb158b25a798db3ef676f
Milan Nellestijn <323101@student.mboutrecht.nl>
2019-05-29 16:41:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,61 +18,9 @@
   echo "Change";
   echo "User-2 was here, all base are belong to us";
   echo "rra-enz.. was also here, I take base belong to me";
-<<<<<<< HEAD
   echo "Leon is hier";
   echo "olaf was hier";
-=======
-
-  echo "Leon is hier";
-  echo "olaf was hier";
-  echo "User-2 rules@@@@@@@@@@@@@@@@@";
-  echo "Nieuw regel";
-  echo "User-2 rules";
-  echo "Dit is een test";
-  echo "Naam veranderd";
-  echo "Weer een verandering";
-  echo "Change";
-  echo "User-2 was here, all base are belong to us";
-  echo "rra-enz.. was also here, I take base belong to me";
-  echo "User-2 rules";
-  echo "Dit is een test";
-  echo "Naam veranderd";
-  echo "Weer een verandering";
-  echo "Change";
-  echo "User-2 was here, all base are belong to us";
-  echo "rra-enz.. was also here, I take base belong to me";
-  echo "User-2 rules";
-  echo "Dit is een test";
-  echo "Naam veranderd";
-  echo "Weer een verandering";
-  echo "Change";
-  echo "User-2 was here, all base are belong to us";
-  echo "rra-enz.. was also here, I take base belong to me";
-  echo "User-2 rules";
-  echo "Dit is een test";
-  echo "Naam veranderd";
-  echo "Weer een verandering";
-  echo "Change";
-  echo "User-2 was here, all base are belong to us";
-  echo "rra-enz.. was also here, I take base belong to me";
-  echo "User-2 rules";
-  echo "Dit is een test";
-  echo "Naam veranderd";
-  echo "Weer een verandering";
-  echo "Change";
-  echo "User-2 was here, all base are belong to us";
-  echo "rra-enz.. was also here, I take base belong to me";
-  echo "User-2 rules";
-  echo "Dit is een test";
-  echo "Naam veranderd";
-  echo "Weer een verandering";
-  echo "Change";
-  echo "User-2 was here, all base are belong to us";
-  echo "rra-enz.. was also here, I take base belong to me";
-  echo "User-2 rules";
-  echo "jay owen";
->>>>>>> origin/master
-
+ echo "iemand haal jou neefje op";
 
 echo "Tralalala dingdong"
 
