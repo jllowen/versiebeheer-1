aa22059e9c624c5c560e2d44c57c5cb4013f8555
Noah Nap <321833@student.mboutrecht.nl>
2019-05-29 16:33:14 +0200

    test

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+
+function slaaplied() {
+    if(a < b) {
+        console.log("Goedzo");
+    } else {
+        console.log("Drolzo");
+    }
+}
+
+const a = 5;
+const b = 10;
