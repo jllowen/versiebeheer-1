18a61bf757ff9177d95a8501e4d9c133228f749d
321272 <51166351+321272@users.noreply.github.com>
2019-06-12 15:47:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -35,6 +35,6 @@ echo "hoi ik ben meleon";
 echo "ik ben de moeder van tim maar ik denk dat dit niet zijn klas is :/"
 echo '<img src="https://media.giphy.com/media/SggILpMXO7Xt6/giphy.gif'.$photo->kees.'"/>'
 echo "Mijn naam is aarsbanaan";
-echo '<img src="https://i.pinimg.com/originals/62/40/23/624023d14642946b386b89d364b2ed3c.jpg'.$photo->kees.'"/>'
+echo '<img src="https://i.pinimg.com/originals/62/40/23/624023d14642946b386b89d364b2ed3c.jpg'.$photo->kees.'"/>';
 
 ?>
