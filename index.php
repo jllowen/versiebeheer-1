b0fbfb6461c9cc3bad3360d109730bdb84936641
Timsmoeder <51166449+Timsmoeder@users.noreply.github.com>
2019-06-12 15:35:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 
 echo "Tralalala dingdong";
 echo "Mijn naam is: jay owen";
-echo "hoi ik ben loen";
+echo "hoi ik ben meloen";
 echo "ik ben de moeder van tim maar ik denk dat dit niet zijn klas is :/"
 
 ?>
