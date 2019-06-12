ddcc81507c6b5611bf56cc344d75af1ff0138c97
rollerorange <joery1999@gmail.com>
2019-06-12 15:53:25 +0200

    hoi

---
+++ class_text - kopie (18) kopie.php
@@ -0,0 +1,21 @@
+<?php
+  class Tekst {
+    // Fields
+    var $wattekst = "Dit is een tekst met een alternative fact erin.";
+    var $nieuwetekst = "Dit is de derde commit die ik maak";
+    var $tekst1 = "Dit is de vierde commit die ik maak";
+    var $tekst2 = "Dit is de vijfde commit die ik maak";
+    var $tekst3 = "Dit is de zesde commit die ik maak";
+    var $tekst4 = "Dit is de zevende commit die ik maak";
+    var $tekst5 = "We gaan vandaag samenwerken";
+    var $tekst6 = "Nieuwe wijziging";
+    var $tekst7 = "uithof";
+    var $tekst8 = "In de trein";
+    var $tekst9 = "Nieuw commit om te kunnen samenwerken";
+    var $tekst10 = "hallo jonge lui";
+    var $text11 = "kwaak kwaak" ;
+    var $text12 = "Haal jou neefje op lowen" ;
+   
+    
+  }
+?>
