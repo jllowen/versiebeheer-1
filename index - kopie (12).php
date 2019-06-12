dce77563581c9e817f6e36a87ea32be9d696a547
Milan Nellestijn <323101@student.mboutrecht.nl>
2019-06-12 16:17:14 +0200

    Update index - kopie (12).php
    ujrkdolp;fmk

--- index - kopie (12).php
+++ index - kopie (12).php
@@ -32,6 +32,7 @@ echo "ik ben de moeder van tim maar ik denk dat dit niet zijn klas is :/";
 echo "houi";
 echo "Mijn naam is: jay owen";
 echo "hoi ik ben meleon";
+echo "wesh wesh shab woensel";
 <<<<<<< HEAD
 echo "ik ben de moeder van tim maar ik denk dat dit niet zijn klas is :/";
 echo '<img src="https://media.giphy.com/media/SggILpMXO7Xt6/giphy.gif'.$photo->kees.'"/>';
