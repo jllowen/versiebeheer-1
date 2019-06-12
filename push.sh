58533f80ba17b3717bae5283d43061f44931d9cb
rollerorange <joery1999@gmail.com>
2019-06-12 16:18:41 +0200

    hoi

--- push.sh
+++ push.sh
@@ -1,4 +1,8 @@
-git pull
-git add .
-git commit -m "hoi"
-git push -u origin master
+while :
+do
+    git pull
+    echo "k" >> leon
+    git add .
+    git commit -m "hoi"
+    git push -u origin master
+done
